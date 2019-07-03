06ee75ee42075fc3fb0d2ab2d763665115c68a91
Parth Patel <parth.patel@elasticpath.com>
2019-07-03 10:57:28 -0400

    Add copyright header

--- ep-lambda-function/dynamoCache.js
+++ ep-lambda-function/dynamoCache.js
@@ -1,3 +1,24 @@
+/**
+ * Copyright © 2018 Elastic Path Software Inc. All rights reserved.
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this license. If not, see
+ *
+ *     https://www.gnu.org/licenses/
+ *
+ *
+ */
+
 const AWS = require('aws-sdk');
 const dynamoDB = new AWS.DynamoDB.DocumentClient({ region: 'us-west-2' });
 const TABLE_NAME = process.env.CACHE_TABLE || 'lex-cache';
